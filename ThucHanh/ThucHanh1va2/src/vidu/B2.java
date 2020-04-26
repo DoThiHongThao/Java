fda3daf57ce2180e3594013b73ede2603efd1eef
DoThiHongThao <hongthao2491999@gmail.com>
2020-04-26 22:05:14 +0700

    Update B2.java

--- ThucHanh/ThucHanh1va2/src/vidu/B2.java
+++ ThucHanh/ThucHanh1va2/src/vidu/B2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class B2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner a= new Scanner(System.in);
 		int n;
 		System.out.printf("Nhap so n: ");
